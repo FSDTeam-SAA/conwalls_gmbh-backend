d55c04e0df98933ab906e9e90f574102bfda33ef
Rafiul-Hossain <rafiulhossain4393@gmail.com>
2026-02-19 14:38:50 +0600

    Implement admin user management features with routes and services

--- src/core/app/appRouter.js
+++ src/core/app/appRouter.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authRoutes from '../../entities/auth/auth.routes.js';
 import userRoutes from '../../entities/user/user.routes.js';
 import systemSettingRoutes from '../../entities/systemSetting/systemSetting.routes.js';
+import adminUserRoutes from "../../entities/adminUser/adminUser.routes.js";
 
 const router = express.Router();
 
 router.use('/v1/auth', authRoutes);
 router.use('/v1/user', userRoutes);
 router.use('/v1/system-setting', systemSettingRoutes);
+router.use("/v1/admin/users", adminUserRoutes);
 
 export default router;
