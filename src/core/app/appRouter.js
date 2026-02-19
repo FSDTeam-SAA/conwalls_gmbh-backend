e1365d95101b177f3660e84d0b64bb9726f1d540
Wrong1234 <mahabur24cse@gmail.com>
2026-02-19 15:42:28 +0600

    feat: user session

--- src/core/app/appRouter.js
+++ src/core/app/appRouter.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authRoutes from '../../entities/auth/auth.routes.js';
 import userRoutes from '../../entities/user/user.routes.js';
 import systemSettingRoutes from '../../entities/systemSetting/systemSetting.routes.js';
+import { sessionRouter }  from '../../entities/session/session.route.js'; // import session routes
 
 const router = express.Router();
 
 router.use('/v1/auth', authRoutes);
 router.use('/v1/user', userRoutes);
 router.use('/v1/system-setting', systemSettingRoutes);
+router.use('/v1/insight-engine', sessionRouter); // add this line for session routes
 
 export default router;
---
+++ src/entities/session/session.service.js
@@ -0,0 +1,54 @@
+import InsightEngine from "./session.model.js";
+import { createFilter, createPaginationInfo } from "../../lib/pagination.js";
+
+export const submitInsightEngine = async ({ createdBy, payload }) => {
+
+  const doc = await InsightEngine.create({
+    createdBy: createdBy || null,
+    ...payload,
+  });
+
+  return doc;
+};
+
+export const getAllInsightEngines = async ({ page = 1, limit = 10, search, date }) => {
+  const filter = createFilter(search, date);
+
+  const total = await InsightEngine.countDocuments(filter);
+
+  const items = await InsightEngine.find(filter)
+    .select("-__v")
+    .sort({ createdAt: -1 })
+    .skip((page - 1) * limit)
+    .limit(limit);
+
+  const paginationInfo = createPaginationInfo(page, limit, total);
+
+  return { items, paginationInfo };
+};
+
+export const getInsightEngineById = async (id) => {
+  const item = await InsightEngine.findById(id).select("-__v");
+  if (!item) throw new Error("InsightEngine not found");
+  return item;
+};
+
+export const updateInsightEngine = async ({ id, payload }) => {
+  if (!payload || Object.keys(payload).length === 0) {
+    throw new Error("Update payload is required");
+  }
+
+  const updated = await InsightEngine.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  }).select("-__v");
+
+  if (!updated) throw new Error("InsightEngine not found");
+  return updated;
+};
+
+export const deleteInsightEngine = async (id) => {
+  const deleted = await InsightEngine.findByIdAndDelete(id);
+  if (!deleted) throw new Error("InsightEngine not found");
+  return true;
+};
